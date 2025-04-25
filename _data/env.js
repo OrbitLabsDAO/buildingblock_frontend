9504f66aa8bc7cbfe6a0a78bdf264a0fed60c381
Christopher McCreadie <cjmccreadie@jellyfin.local>
2025-04-25 23:15:36 +0700

    updates

--- _data/env.js
+++ _data/env.js
@@ -7,4 +7,11 @@ module.exports = {
   YEAR: _YEAR,
   APIURL: process.env.APIURL,
   ADMINURL: process.env.ADMINURL,
+  FRONTENDCODEURL: "https://github.com/OrbitLabsDAO/buildingblock_frontend/",
+  ADMINCODEURL: "https://github.com/OrbitLabsDAO/buildingblock_admin",
+  CROWDFUNDCODEURL: "https://github.com/OrbitLabsDAO/buildingblock-crowdfund",
+  PAYMENTCODEURL: "https://github.com/OrbitLabsDAO/paymev2",
+  CROWDFUNDDEMOURL: "https://crowdfund.buildblock.app",
+  ADMINDEMOURL: "https://admin.buildblock.app",
+  PAYMENTDEMOURL: "",
 };
